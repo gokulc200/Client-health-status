15dc44e88ccb19301f1097f19e63c70c5649dd51
Gokul <121416463+gokulc200@users.noreply.github.com>
2023-03-24 17:41:23 +0530

    Update UserDetails.js

--- UserDetails/UserDetails.js
+++ UserDetails/UserDetails.js
@@ -1,4 +1,9 @@
 let quote = document.getElementById("quote");
+let checkupElement1 = document.getElementById("next-checkup1");
+let checkupElement2 = document.getElementById("next-checkup2");
+var date = new Date();
+var current_date = (date.getDate()) + "-" + (date.getMonth() + 2) + "-" + date.getFullYear();
+
 let quotesList = [`“Wrinkles will only go where the smiles have been😊”`,
     `“Count your age by friends, not years. Count your life by smiles😊, not tears”`,
     `“Have a good day❤️”`, `“Hope you are doing well!❤️”`, `“One are so old as those who have outlived enthusiasm🌟”`,
@@ -10,3 +15,5 @@ let noOfMessageInChatList = quotesList.length;
 let chatBotMessage = quotesList[Math.ceil(Math.random() * noOfMessageInChatList) - 1];
 
 quote.textContent = chatBotMessage;
+checkupElement1.textContent = "Next Checkup : " + current_date;
+checkupElement2.textContent = "Next Checkup : " + current_date;
